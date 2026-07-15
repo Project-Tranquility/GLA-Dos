dac6f78146a834584b24c3aca5ca273e8d0444f9
Lucas <lucas1.eeckhoutte@epitech.eu>
2026-07-15 15:26:53 +0200

    feat!: add first command

--- main.py
+++ main.py
@@ -13,4 +13,8 @@ bot = discord.Bot()
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+@bot.slash_command(name="hello", description="Just a hello word command")
+async def hello(ctx):
+    await ctx.respond("Hello too")
+
 bot.run(discord_token)
